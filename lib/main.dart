eca480037404bcee4bfb1c4bd8d8e7ecdcedbf40
neiljaywarner <njwandroid@gmail.com>
2024-02-03 18:02:05 -0600

    put album class into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'album.dart';
+
 Future<Album> fetchAlbum() async {
   final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 
@@ -14,35 +16,7 @@ Future<Album> fetchAlbum() async {
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  const Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'userId': int userId,
-        'id': int id,
-        'title': String title,
-      } =>
-        Album(
-          userId: userId,
-          id: id,
-          title: title,
-        ),
-      _ => throw const FormatException('Failed to load album.'),
-    };
+    throw Exception('Failed to load album.dart');
   }
 }
 
